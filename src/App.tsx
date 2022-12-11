ea81a1656bd3967a6d3abf3ccf41dab990616fda
Nagasundar P <sundar@Nagasundars-MacBook-Air.local>
2022-12-11 14:45:03 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,38 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState} from 'react';
 import {Button, Form} from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  const [search, setSearch] = useState<string>('');
-  const [data, setData] = useState<any>([]);
+  // eslint-disable-next-line 
+  const [key, setKey] = useState<string>('');
+  const [answer, setAnswer] = useState<string>('');
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
+    event.preventDefault();
+    setKey(event.target.value);
+    function getKeyByValue(object: { [x: string]: any; person1?: string; person2?: string; person3?: string; }, value: string) {
+      return Object.keys(object).find(key => object[key] === value);
+    }
+    
+    const map = {"person1": "John", "person2": "Jane", "person3": "Jack"};
+    const answer = (getKeyByValue(map, event.target.value));
+    if (answer === "person1") {
+      setAnswer("person1");
+    }
+    else if (answer === "person2") {
+      setAnswer("person2");
+    }
+    else if (answer === "person3") {
+      setAnswer("person3");
+    }
+    else {
+      setAnswer("Not Found");
+    }
   }
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // console.log(search);
-    fetchData();
-    data.filter((users: any) => {
-      if (users.name.toLowerCase().includes(search.toLowerCase())) {
-        console.log(users.name);
-      }
-    }
-    )
-  }
-  useEffect(() => {
-    const getData = async () => {
-      const dataFromServer = await fetchUsers();
-      setData(dataFromServer);
-    }
-  }, []);
-  
-  const fetchData = async () => {
-    const response = await fetch('http://localhost:3004/users');
-    const data = await response.json();
-}
-  fetchData();
   return (
     <div className="App">
       <h1>Search App</h1>
-      <Form onSubmit={handleSubmit}>
+      <Form>
       <Form.Group className="mb-3" controlId="formBasicSearch">
         <Form.Label>Search Box</Form.Label>
         <Form.Control type="text" placeholder="Enter item to search" onChange={handleChange} />
@@ -48,15 +45,13 @@ function App() {
       </Button>
       </Form>
       <div>
-        <h2>Results</h2>
-        <p></p>
+        <h1>Result</h1>
+        <p>{answer}</p>
       </div>
     </div>
   );
 }
 
 export default App;
-function fetchUsers() {
-  throw new Error('Function not implemented.');
-}
+
 
