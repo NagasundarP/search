d02561d98882e5259b00152586b4d9837a8ff552
Nagasundar P <sundar@Nagasundars-MacBook-Air.local>
2022-12-11 11:20:39 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
+import {Button, Form} from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState<string>('');
+  const [data, setData] = useState<any>([]);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // console.log(search);
+    fetchData();
+    data.filter((users: any) => {
+      if (users.name.toLowerCase().includes(search.toLowerCase())) {
+        console.log(users.name);
+      }
+    }
+    )
+  }
+  useEffect(() => {
+    const getData = async () => {
+      const dataFromServer = await fetchUsers();
+      setData(dataFromServer);
+    }
+  }, []);
+  
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:3004/users');
+    const data = await response.json();
+}
+  fetchData();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search App</h1>
+      <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicSearch">
+        <Form.Label>Search Box</Form.Label>
+        <Form.Control type="text" placeholder="Enter item to search" onChange={handleChange} />
+        <Form.Text className="text-muted">
+          Search for an item
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      </Form>
+      <div>
+        <h2>Results</h2>
+        <p></p>
+      </div>
     </div>
   );
 }
 
 export default App;
+function fetchUsers() {
+  throw new Error('Function not implemented.');
+}
+
